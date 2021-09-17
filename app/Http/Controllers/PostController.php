e81e1e01d85ab43051b06161c9accf0462aa63fd
Ulugbek Turakulov <ulugbek.turaqulov311@gmail.com>
2021-09-17 13:12:33 +0500

    Initial commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\PostRequest;
+use App\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index(Request $request)
+    {
+        $posts = Post::latest();
+
+        if ($request->title) {
+            $posts->where('title', $request->title);
+        }
+
+        if ($request->text) {
+            $posts->where('text', $request->text);
+        }
+
+        if ($request->author) {
+            $posts->where('author', $request->author);
+        }
+
+        if ($request->likes) {
+            $posts->where('likes', $request->likes);
+        }
+
+        if ($request->created_at) {
+            $posts->where('created_at', $request->created_at);
+        }
+
+        return $posts->get();
+    }
+
+    public function store(PostRequest $request)
+    {
+        Post::create($request->validated());
+
+        return response(200);
+    }
+
+    public function update(PostRequest $request, $id)
+    {
+        Post::where('id', $id)->update($request->validated());
+
+        return response(200);
+    }
+
+    public function destroy($id)
+    {
+        $post = Post::findOrFail($id);
+        $post->delete();
+        return response(200);
+    }
+}
